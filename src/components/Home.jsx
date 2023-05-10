c8309df83d4cd8eeff47b3384ba2f68d81315ba0
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2023-05-10 16:59:32 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
               </Box>
               <Box p={2}>
                 <a
-                  href="https://drive.google.com/file/d/1I_KQyMRyony2qDyP_HopPmYdOdkehuYt/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1SlBvMZdMYmoWdmmPGnGEgU57M-Up70gd/view?usp=share_link"
                   target="_blank"
                   rel="noreferrer"
                   style={{ textDecoration: "none" }}
